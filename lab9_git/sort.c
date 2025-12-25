e0fb64645547824599bf9e346cf44b7afb9c8c70
F1nt1337 <slavagekel545@gmail.com>
2025-12-26 02:04:26 +0700

    Final update

--- lab9_git/sort.c
+++ lab9_git/sort.c
@@ -1,6 +1,6 @@
 void sort(int arr[], int n) {
     int temp;
-    for (int i = 0; i < n - 1; i++) { // simple bubble sort
+    for (int i = 0; i < n - 1; i++) { // simple bubble
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 temp = arr[j];
